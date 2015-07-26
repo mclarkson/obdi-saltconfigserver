5994855829b663c7f9c7eb70950e610775391b20
Mark Clarkson <mark.clarkson@smorg.co.uk>
2015-07-26 18:04:26 +0100

    Exceptions to stdout

--- scripts/statelist.sh
+++ scripts/statelist.sh
@@ -64,22 +64,52 @@ import salt.cli
 import salt.config
 import salt.fileserver.gitfs
 
+def ErrorMsg(escape=0):
+    """
+    returns: string
+    
+    simualtes the traceback output and if argemument
+    <escape> set to 1 (true) the string will be
+    converted to fit into html documents without problems.    
+    """
+    import traceback, sys, string
+    
+    type=None
+    value=None
+    tb=None
+    limit=None
+    type, value, tb = sys.exc_info()
+    body = "Traceback (innermost last):\n"
+    list = traceback.format_tb(tb, limit) + traceback.format_exception_only(type, value)
+    body = body + "%-20s %s" % (
+        string.join(list[:-1], ""),
+        list[-1],
+        )
+    if escape:
+        import cgi
+        body = cgi.escape(body)
+    return body
+
 def salt_update():
 
-    salt.fileserver.gitfs.__opts__ = salt.config.master_config(
-            os.environ.get('SALT_MASTER_CONFIG', '/etc/salt/master'))
-
-    pid = os.getpid()
-    result = []
     try:
-        salt.fileserver.gitfs.update()
-        load = {'saltenv':'$ENV'}
-        file = salt.fileserver.gitfs.file_list(load)
-        for i in file:
-            if fnmatch.fnmatch(i, "*.sls"):
-                result.append( salt.fileserver.gitfs.find_file(i,'$ENV') )
-    except KeyboardInterrupt:
-        os.kill(pid, 15)
+        salt.fileserver.gitfs.__opts__ = salt.config.master_config(
+                os.environ.get('SALT_MASTER_CONFIG', '/etc/salt/master'))
+
+        pid = os.getpid()
+        result = []
+        try:
+            salt.fileserver.gitfs.update()
+            load = {'saltenv':'$ENV'}
+            file = salt.fileserver.gitfs.file_list(load)
+            for i in file:
+                if fnmatch.fnmatch(i, "*.sls"):
+                    result.append( salt.fileserver.gitfs.find_file(i,'$ENV') )
+        except KeyboardInterrupt:
+            os.kill(pid, 15)
+    except Exception:
+        print ErrorMsg()
+        sys.exit(1)
 
     return result
 
